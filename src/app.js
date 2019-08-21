b3e16bd51cb810104d260b4112da72ceb75b57ac
Rafael Alcantara <rafael_william_@hotmail.com>
2019-08-20 23:00:44 -0300

    Início do projeto.

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import routes from './routes';
+
+class App {
+  constructor() {
+    this.server = express();
+    this.useMiddlewares();
+    this.useRoutes();
+  }
+
+  useMiddlewares() {
+    this.server.use(json());
+  }
+
+  useRoutes() {
+    this.server.use(routes);
+  }
+}
+
+export default new App().server;
