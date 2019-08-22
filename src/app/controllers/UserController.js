919c149ec9f41e526eac9d62aeece2bc8633d27f
Rafael Alcantara <rafael_william_@hotmail.com>
2019-08-21 21:46:43 -0300

    Criptografia de senha e alteração de usuário.

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -6,6 +6,18 @@ class UserController {
 
     return res.json(user);
   }
+
+  async update(req, res) {
+    const user = await User.findByPk(req.params.id);
+
+    const { id, name, email } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+    });
+  }
 }
 
 export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,6 @@ import UserController from './app/controllers/UserController';
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.put('/users/:id', UserController.update);
 
 export default routes;
