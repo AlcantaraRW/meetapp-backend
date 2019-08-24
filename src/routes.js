2bd417d226e63bc671e61440e2255eee42ade038
Rafael Alcantara <rafael_william_@hotmail.com>
2019-08-24 19:01:43 -0300

    Lista de Meetups organizadas pelo usuário logado.

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
 import FileController from './app/controllers/FileController';
 import MeetupController from './app/controllers/MeetupController';
+import OrganizingController from './app/controllers/OrganizingController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -24,5 +25,7 @@ routes.get('/meetups', MeetupController.index);
 routes.post('/meetups', MeetupController.store);
 routes.put('/meetups/:id', MeetupController.update);
 routes.delete('/meetups/:id', MeetupController.delete);
+
+routes.get('/organizing', OrganizingController.index);
 
 export default routes;
